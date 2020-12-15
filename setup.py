094b5b3f48da9e1c97507ff006a09599470d54cd
Noflame <linjuang@gmail.com>
2020-12-15 18:10:53 +0800

    Chore: Remove comment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py_shotgun", # Replace with your own username
+    name="py_shotgun",
     version="0.0.1",
     author="Noflame Lin",
     author_email="linjuang@gmail.com",
